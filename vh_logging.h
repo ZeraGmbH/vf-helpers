168dea699145450c4ef951336f3c3be840853fd3
Samuel Stirtzel <s.stirtzel@googlemail.com>
2015-11-03 15:48:37 +0100

    Added custom macro alias for Q_ASSERT_X
    The where parameter is redundant with the __FUNCTION__ definition
    
    Signed-off-by: Samuel Stirtzel <s.stirtzel@googlemail.com>

--- vh_logging.h
+++ vh_logging.h
@@ -9,5 +9,8 @@
 #define vCDebug(X) qCDebug(X)
 #endif
 
+// if __FUNCTION__ does not work with your comiler then add some conditions here
+#define VF_ASSERT(test, what) Q_ASSERT_X(test, __FUNCTION__, what)
+
 #endif // VH_LOGGING
 
