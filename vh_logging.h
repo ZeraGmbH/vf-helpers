5a4d933190674f2314c053c7b62ad4eeedc2aa35
Samuel Stirtzel <s.stirtzel@googlemail.com>
2018-06-19 09:35:16 +0200

    Moved logging category name definition to vh_logging.h
    Signed-off-by: Samuel Stirtzel <s.stirtzel@googlemail.com>

--- vh_logging.h
+++ vh_logging.h
@@ -13,5 +13,36 @@
 #define VF_ASSERT(test, what) Q_ASSERT_X(test, __PRETTY_FUNCTION__, what)
 #define QStringC(X) (X).toStdString().c_str()
 
-#endif // VH_LOGGING
+#if defined(VEIN_COLORS_SUPPORT)
+# define VEIN_DEBUGNAME_EVENT "\e[1;34m<Vein.Event>\033[0m"
+# define VEIN_DEBUGNAME_COMPONENT "\e[1;34m<Vein.Event>\033[0m"
+# define VEIN_DEBUGNAME_NET "\e[1;32m<Vein.Network>\033[0m"
+# define VEIN_DEBUGNAME_NET_VERBOSE "\e[0;32m<Vein.Network>\033[0m"
+# define VEIN_DEBUGNAME_NET_INTRO "\e[1;35m<Vein.Network.Introspection>\033[0m"
+# define VEIN_DEBUGNAME_NET_INTRO_VERBOSE "\e[0;35m<Vein.Network.Introspection>\033[0m"
+# define VEIN_DEBUGNAME_SCRIPT "\e[1;37m<Vein.Script>\033[0m"
+# define VEIN_DEBUGNAME_QML "\e[1;37m<Vein.Api.QML>\033[0m"
+# define VEIN_DEBUGNAME_QML_INTROSPECTION "\e[1;37m<Vein.Api.QML.Introspection>\033[0m"
+# define VEIN_DEBUGNAME_QML_VERBOSE "\e[0;37m<Vein.Api.QML>\033[0m"
+# define VEIN_DEBUGNAME_STORAGE_HASH "\033[1;36m<Vein.Storage.Hash>\033[0m"
+# define VEIN_DEBUGNAME_STORAGE_HASH_VERBOSE "\033[0;36m<Vein.Storage.Hash>\033[0m"
+# define VEIN_DEBUGNAME_NET_TCP "\e[1;33m<Vein.Network.Tcp>\033[0m"
+# define VEIN_DEBUGNAME_NET_TCP_VERBOSE "\e[0;33m<Vein.Network.Tcp>\033[0m"
+#else
+# define VEIN_DEBUGNAME_EVENT "<Vein.Event>"
+# define VEIN_DEBUGNAME_COMPONENT "<Vein.Event>"
+# define VEIN_DEBUGNAME_NET "<Vein.Network>"
+# define VEIN_DEBUGNAME_NET_VERBOSE "<Vein.Network>"
+# define VEIN_DEBUGNAME_NET_INTRO "<Vein.Network.Introspection>"
+# define VEIN_DEBUGNAME_NET_INTRO_VERBOSE "<Vein.Network.Introspection>"
+# define VEIN_DEBUGNAME_SCRIPT "<Vein.Script>"
+# define VEIN_DEBUGNAME_QML "<Vein.Api.QML>"
+# define VEIN_DEBUGNAME_QML_INTROSPECTION "<Vein.Api.QML.Introspection>"
+# define VEIN_DEBUGNAME_QML_VERBOSE "<Vein.Api.QML>"
+# define VEIN_DEBUGNAME_STORAGE_HASH "<Vein.Storage.Hash>"
+# define VEIN_DEBUGNAME_STORAGE_HASH_VERBOSE "<Vein.Storage.Hash>"
+# define VEIN_DEBUGNAME_NET_TCP "<Vein.Network.Tcp>"
+# define VEIN_DEBUGNAME_NET_TCP_VERBOSE "<Vein.Network.Tcp>"
+#endif //defined(VEIN_COLORS_SUPPORT)
 
+#endif // VH_LOGGING
